9769d4539135bdcce367bdab4d7df47905080263
RoastedBear <kinim329@gmail.com>
2026-03-25 10:57:18 +0900

    feat : 3월 25일 kata 백준7287

---
+++ src/main/java/com/sparta/baekjoon/B7287.java
@@ -0,0 +1,8 @@
+package com.sparta.baekjoon;
+
+public class B7287 {
+    public static void main(String[] args) {
+        System.out.println("15");
+        System.out.println("kinim329");
+    }
+}
