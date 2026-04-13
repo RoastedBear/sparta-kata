da80db41ed5713901b09d6a49f6925f2b41e6a53
RoastedBear <kinim329@gmail.com>
2026-04-13 10:24:58 +0900

    feat : 4월 13일 kata 백준10872

---
+++ src/main/java/com/sparta/baekjoon/B10872.java
@@ -0,0 +1,15 @@
+package com.sparta.baekjoon;
+
+import java.util.Scanner;
+
+public class B10872 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = 1;
+        for (int i = 1; i <= n; i++) {
+            x = x*i;
+        }
+        System.out.println(x);
+    }
+}
