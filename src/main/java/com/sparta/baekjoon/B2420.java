39cbb47bfde846d7aa7208c3a0400ce118d5d83a
RoastedBear <kinim329@gmail.com>
2026-04-16 19:08:45 +0900

    feat : 4월 16일 kata 백준2420

---
+++ src/main/java/com/sparta/baekjoon/B2420.java
@@ -0,0 +1,14 @@
+package com.sparta.baekjoon;
+
+import java.util.Scanner;
+
+public class B2420 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long N = sc.nextLong();
+        long M = sc.nextLong();
+
+        System.out.println(Math.abs(N - M));
+    }
+}
