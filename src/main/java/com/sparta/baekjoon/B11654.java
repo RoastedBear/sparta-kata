5223450c596ca68e000f938256c4643ca89c11f4
RoastedBear <kinim329@gmail.com>
2026-04-22 19:48:46 +0900

    feat : 4월 22일 kata 백준2743

---
+++ src/main/java/com/sparta/baekjoon/B11654.java
@@ -0,0 +1,11 @@
+package com.sparta.baekjoon;
+
+import java.util.Scanner;
+
+public class B11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        System.out.println((int) ch);
+    }
+}
---
+++ src/main/java/com/sparta/baekjoon/B2743.java
@@ -0,0 +1,12 @@
+package com.sparta.baekjoon;
+
+import java.util.Scanner;
+
+public class B2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        int b = a.length();
+        System.out.println(b);
+    }
+}
