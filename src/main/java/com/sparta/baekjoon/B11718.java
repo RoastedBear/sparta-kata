3a07d42941e2a95577f7afe8e8fb24f4800bc011
RoastedBear <kinim329@gmail.com>
2026-04-28 23:51:06 +0900

    feat : 4월 27일 kata 백준11718

---
+++ src/main/java/com/sparta/baekjoon/B11718.java
@@ -0,0 +1,14 @@
+package com.sparta.baekjoon;
+
+import java.util.Scanner;
+
+public class B11718 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            String a = sc.nextLine();
+            System.out.println(a);
+        }
+    }
+}
