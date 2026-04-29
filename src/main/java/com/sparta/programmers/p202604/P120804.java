f6be4b307b0fb0ff4e11b4becb64ba13c6eb4f38
RoastedBear <kinim329@gmail.com>
2026-04-29 23:38:49 +0900

    feat : 4월 28일 kata 프로그래머스120804

---
+++ src/main/java/com/sparta/programmers/p202604/P120804.java
@@ -0,0 +1,12 @@
+package com.sparta.programmers.p202604;
+
+import java.util.Scanner;
+
+public class P120804 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(num1*num2);
+    }
+}
