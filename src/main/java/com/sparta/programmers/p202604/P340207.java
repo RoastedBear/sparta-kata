81c8679340864b30caea673da65ff8680e2e8b9a
RoastedBear <kinim329@gmail.com>
2026-04-30 23:17:39 +0900

    feat : 4월 29일 kata 프로그래머스340207

---
+++ src/main/java/com/sparta/programmers/p202604/P340207.java
@@ -0,0 +1,9 @@
+package com.sparta.programmers.p202604;
+
+public class P340207 {
+    public static void main(String[] args) {
+        String message = " Let's go! ";
+        System.out.println("3\n 2\n 1");
+        System.out.println(message);
+    }
+}
