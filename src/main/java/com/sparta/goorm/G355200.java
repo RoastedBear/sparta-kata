49b31af8c19b2fb1dfe353bff19dbd0bb2f139ce
RoastedBear <kinim329@gmail.com>
2026-05-05 23:50:00 +0900

    5월 5일 kata 구름 355200

--- src/main/java/com/sparta/goorm/G355200.java
+++ src/main/java/com/sparta/goorm/G355200.java
@@ -1,4 +1,27 @@
 package com.sparta.goorm;
 
+import java.util.Scanner;
+
 public class G355200 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long am = sc.nextLong();
+        long bm = sc.nextLong();
+        long cm = sc.nextLong();
+
+        long a = am * bm;
+        long b = cm;
+
+        long x = a;
+        long y = b;
+
+        while (y != 0) {
+            long c = x % y;
+            x = y;
+            y = c;
+        }
+
+        System.out.println((a / x) + " " + (b / x));
+    }
 }
