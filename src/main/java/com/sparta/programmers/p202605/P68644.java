69b1e8bd240716c6a11ffa9f5e8c50441bb553cf
RoastedBear <kinim329@gmail.com>
2026-05-16 17:37:00 +0900

    feat : 5월 15일 kata 프로그래머스68644

---
+++ src/main/java/com/sparta/programmers/p202605/P68644.java
@@ -0,0 +1,19 @@
+package com.sparta.programmers.p202605;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class P68644 {
+    class Solution {
+        public int[] solution(int[] numbers) {
+            Set<Integer> set = new HashSet<>();
+
+            for (int i = 0; i < numbers.length; i++) {
+                for (int j = i + 1; j < numbers.length; j++) {
+                    set.add(numbers[i] + numbers[j]);
+                }
+            }
+            return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+        }
+    }
+}
