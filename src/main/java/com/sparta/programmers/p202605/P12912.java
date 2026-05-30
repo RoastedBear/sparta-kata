14e8dbc0aad4264d112dc2f08006acebdbb009c2
RoastedBear <kinim329@gmail.com>
2026-05-30 12:07:00 +0900

    feat : 5월 30일 kata 프로그래머스12912

---
+++ src/main/java/com/sparta/programmers/p202605/P12912.java
@@ -0,0 +1,22 @@
+package com.sparta.programmers.p202605;
+
+import java.util.Scanner;
+
+public class P12912 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        long answer = 0;
+
+        int start = Math.min(a,b);
+        int end = Math.max(a, b);
+
+        for(int i = start; i <= end; i++){
+            answer += i;
+        }
+
+        System.out.println(answer);
+    }
+}
