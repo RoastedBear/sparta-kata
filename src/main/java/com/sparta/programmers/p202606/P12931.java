d16f78de51a0c710e7fd5549394462d0d11a9f75
RoastedBear <kinim329@gmail.com>
2026-06-02 18:23:27 +0900

    6월 2일 프로그래머스 12931

---
+++ src/main/java/com/sparta/programmers/p202606/P12931.java
@@ -0,0 +1,19 @@
+package com.sparta.programmers.p202606;
+
+import java.util.Scanner;
+
+public class P12931 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int answer = 0;
+
+        while (n > 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+
+        System.out.println(answer);
+    }
+}
