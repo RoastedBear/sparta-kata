76ea763e4cb537fc319665933b770bc48ebf4584
RoastedBear <kinim329@gmail.com>
2026-06-04 00:30:23 +0900

    6월 4일 프로그래머스 134240

--- src/main/java/com/sparta/programmers/p202606/P134240.java
+++ src/main/java/com/sparta/programmers/p202606/P134240.java
@@ -1,4 +1,26 @@
 package com.sparta.programmers.p202606;
 
+import java.util.Scanner;
+
 public class P134240 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] food = new int[n];
+        for (int i = 0; i < n; i++) {
+            food[i] = sc.nextInt();
+        }
+
+        StringBuilder left = new StringBuilder();
+        for (int i = 1; i < food.length; i++) {
+            int count = food[i] / 2;
+
+            for (int j = 0; j < count; j++) {
+                left.append(i);
+            }
+        }
+        String result = left.toString() + "0" + left.reverse().toString();
+        System.out.println(result);
+    }
+
 }
