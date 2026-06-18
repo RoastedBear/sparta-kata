5c31e5fd2c69cdd7e31a645055104c5812157fc1
RoastedBear <kinim329@gmail.com>
2026-06-19 02:11:46 +0900

    6월 19일 프로그래머스 120834

---
+++ src/main/java/com/sparta/programmers/p202606/P120834.java
@@ -0,0 +1,64 @@
+package com.sparta.programmers.p202606;
+
+import java.util.Scanner;
+
+public class P120834 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int age = sc.nextInt();
+        String a = String.valueOf(age);
+        String answer = "";
+
+        for (int i = 0; i < a.length(); i++) {
+            int b = a.charAt(i) - '0';
+            answer += (char) ('a' + b);
+        }
+
+        System.out.println(answer);
+    }
+}
+/*
+문제 설명
+
+우주여행을 하던 머쓱이는 엔진 고장으로
+PROGRAMMERS-962 행성에 불시착하게 됐습니다.
+
+입국심사에서 나이를 말해야 하는데,
+PROGRAMMERS-962 행성에서는 나이를 알파벳으로 말하고 있습니다.
+
+a는 0, b는 1, c는 2, ..., j는 9입니다.
+
+예를 들어 23살은 cd, 51살은 fb로 표현합니다.
+
+나이 age가 매개변수로 주어질 때
+PROGRAMMERS-962식 나이를 return하도록
+solution 함수를 완성해주세요.
+
+제한사항
+
+age는 자연수입니다.
+age ≤ 1,000
+PROGRAMMERS-962 행성은 알파벳 소문자만 사용합니다.
+
+입출력 예
+
+age     result
+23      "cd"
+51      "fb"
+100     "baa"
+
+입출력 예 설명
+
+입출력 예 #1
+
+age가 23이므로 "cd"를 return합니다.
+
+입출력 예 #2
+
+age가 51이므로 "fb"를 return합니다.
+
+입출력 예 #3
+
+age가 100이므로 "baa"를 return합니다.
+*/
